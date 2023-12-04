34b8b9ada9ec87e4d210027d83c994ca9a8caac6
lukin921 <lukin921@liu.student.se>
2023-12-04 19:30:22 +0100

    split w/o parameter removes splits on all whitespaces (even consecutive)

--- day04/day04-p2.py
+++ day04/day04-p2.py
@@ -1,10 +1,8 @@
 file = open("input.txt", "r")
 scratches, card_matches, used = dict(), [], 0
 for i, line in enumerate(file):
-    line = line.strip().replace('  ', ' ').split(':')[1].split('|')
-    winning_nums, scratch_nums = line[0].split(' '), line[1].split(' ')
-    winning_nums = [int(x) for x in winning_nums if x != '']
-    scratch_nums = [int(x) for x in scratch_nums if x != '']
+    line = line.strip().split(':')[1].split('|')
+    winning_nums, scratch_nums = line[0].split(), line[1].split()
     matches = [n for n in scratch_nums if n in winning_nums]
     card_matches.append(len(matches) if matches else 0)
     scratches[i] = scratches.get(i, 0) + 1
