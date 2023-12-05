ce6a1f31a6a3a0242b84cdc815f8854439b79c5c
lukin921 <lukin921@liu.student.se>
2023-12-05 22:13:21 +0100

    p2: heuristic that searches with descending step intervals

--- day05/day05-p2.py
+++ day05/day05-p2.py
@@ -6,7 +6,7 @@ for i, line in enumerate(file):
     elif line == '\n': # new line, expect new map next iteration
         expect_new_map = True
     elif expect_new_map: # new map
-        expect_new_map = False
+        expect_new_map, expect_new_num = False, True
         new_map = line.strip().replace(' map:', '').split('-to-')
         ranges.append([])
     else: # range
@@ -14,14 +14,21 @@ for i, line in enumerate(file):
         ranges[-1].append(map_range)
 def find_min_loc():
     seed_ranges = [(seeds[i], seeds[i] + seeds[i+1]) for i in range(0, len(seeds), 2)]
-    for loc_val in range(max([r[0] + r[2] for r in ranges[-1]])):
-        curr = loc_val
-        for range_ in ranges[::-1]:
-            for r in range_: 
-                if curr >= r[0] and curr < r[0] + r[2]:
-                    curr = r[1] + (curr - r[0])
+    steps, bounds = [10**n for n in reversed(range(6))], (0, max([r[0] + r[2] for r in ranges[-1]]))
+    for step in steps:
+        for loc_val in range(*bounds, step):
+            curr = loc_val
+            for range_ in ranges[::-1]:
+                for r in range_: 
+                    if curr >= r[0] and curr < r[0] + r[2]:
+                        curr = r[1] + (curr - r[0])
+                        break
+            for seed_range in seed_ranges:
+                if curr >= seed_range[0] and curr <= seed_range[1]:
+                    bounds = (loc_val - step, loc_val)
                     break
-        for seed_range in seed_ranges:
-            if curr >= seed_range[0] and curr <= seed_range[1]:
-                return loc_val
+            else:
+                continue
+            break
+    return bounds[1]
 print(find_min_loc())
