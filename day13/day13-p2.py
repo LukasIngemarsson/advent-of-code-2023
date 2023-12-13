dfa374ab857add9f2b8d5bd9abca870f6f876d44
lukin921 <lukin921@liu.student.se>
2023-12-13 20:10:43 +0100

    day13 done - revenge from 2022 >:D

--- day13/day13-p2.py
+++ day13/day13-p2.py
@@ -1,22 +1,39 @@
 def find_pattern(grid, axis) -> int:
     if axis == 'x':
         for i in range(len(grid)-1):
-            offx = 0
+            offx, smudge = 0, False
             while i-offx >= 0 and i+offx+1 < len(grid):
-                if not grid[i-offx] == grid[i+offx+1]:
-                    break
-                offx += 1
+                for a, b in zip(grid[i-offx], grid[i+offx+1]):
+                    if a != b:
+                        if not smudge:
+                            smudge = True
+                        else:
+                            break
+                else:
+                    offx += 1
+                    continue
+                break
             else:
-                return i+1
+                if smudge:
+                    return i+1
     elif axis == 'y':
         for i in range(len(grid[0])-1):
-            offy = 0
+            offy, smudge = 0, False
             while i-offy >= 0 and i+offy+1 < len(grid[0]):
-                if not [r[i-offy] for r in grid] == [r[i+offy+1] for r in grid]:
-                    break
-                offy += 1
+                for a, b in zip([r[i-offy] for r in grid], [r[i+offy+1] for r in grid]):
+                    if a != b:
+                        if not smudge:
+                            smudge = True
+                        else:
+                            break
+                else:
+                    offy += 1
+                    continue
+                break
             else:
-                return i+1
+                if smudge:
+                    print('c', i+1)
+                    return i+1
     return 0
 
 file = open("input.txt", "r")
