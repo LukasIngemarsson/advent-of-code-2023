65f740ed0c523499817d20f51c8589ee7bcb5b1e
lukin921 <lukin921@student.liu.se>
2023-12-15 16:38:25 +0100

    day15 p1 done

---
+++ day15/day15-p1.py
@@ -0,0 +1,12 @@
+def hash_algorithm(ch):
+    return (ch * 17) % 256
+
+ans = 0
+with open("input.txt") as f:
+    sequences = [list(x) for x in f.readline().split(',')]
+    for seq in sequences:
+        curr = 0
+        for ch in seq:
+            curr = hash_algorithm(curr + ord(ch))
+        ans += curr
+print(ans)
